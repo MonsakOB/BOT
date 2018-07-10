1861a3a5d783b29c6eb3e2358307bb06b71966db
MonsakOB <40932781+MonsakOB@users.noreply.github.com>
2018-07-10 23:14:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,54 +1,7 @@
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
-39
-40
-41
-42
-43
-44
-45
-46
-47
-48
 <?php
  
-$strAccessToken = "ACCESS_TOKEN";
+$strAccessToken = "ZpH0ADGozQjamp48+PqkPEbl5AX6MaCsm4vwe1F8ut4qQEo0rAcomiCqIMpIvcRpMRYroHjqfoXdaki9gQ2Vf4ddeEWTlNtywhdeYgTmzHS3qwjgHKnZ+BB6bM4S47ME42qM09DW1V9BNz7X7yXQqwdB04t89/1O/w1cDnyilFU=
+";
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
